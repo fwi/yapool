66fa7082c6012a292d157df3e776c6196adf9ccd
frederik.wiers <frederik.wiers@gmail.com>
2013-11-11 20:37:53 +0000

    - Make query formatting for loading queries less strict and easier to use. - Add unit test for prioritizing and filtering on suffix in BeanConfig.

--- yapool/yapool-examples/src/main/resources/example-qc-data.sql
+++ yapool/yapool-examples/src/main/resources/example-qc-data.sql
@@ -1,10 +1,10 @@
 --[]
+-- HSQL can handle several insert statements executed as one statement
 insert into t (name) values ('Donald Duck'); 
 insert into t (name) values ('Mickey Mouse');
 insert into t (name) values ('Marvin the Martian');
---[/]
 --[]
+-- Just for example, split the batch in two
 insert into t (name) values ('Woody Pride'); 
 insert into t (name) values ('Buzz Lightyear');
 insert into t (name) values ('Jessica Jane Pride');
---[/]

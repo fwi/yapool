4f6189f5e4621ba7a47011704587b04318bc3cdc
frederik.wiers@gmail.com <frederik.wiers@gmail.com@7bad2fd1-59a9-c3f4-e3ee-3f43f07789af>
2013-12-01 23:33:35 +0000

    Release 0.9.1

---
+++ tags/yapool/0.9.1/yapool-core/src/main/java/nl/fw/yapool/sql/IQueryCache.java
@@ -0,0 +1,35 @@
+package nl.fw.yapool.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import nl.fw.yapool.IPoolListener;
+
+/**
+ * A query cache that closes queries when the pool destroys connections.
+ * This query cache must be registered as a pool event listener to function.
+ * <p>
+ * Statements returned by this cache may not be closed 
+ * (see {@link DbConn#DbConn(SqlPool, IQueryCache)} which provides this function).
+ * A query builder is used to create statement-objects.
+ * See also {@link SimpleQueryCache} and {@link SimpleQueryBuilder}
+ * @author Fred
+ *
+ */
+public interface IQueryCache extends IPoolListener {
+	
+	/**
+	 * Creates a prepared statement for the connection or re-uses one from cache.
+	 */
+	PreparedStatement getQuery(Connection c, String queryName) throws SQLException;
+	/**
+	 * Creates a named parameter statement for the connection or re-uses one from cache.
+	 */
+	NamedParameterStatement getNamedQuery(Connection c, String queryName) throws SQLException;
+	
+	/**
+	 * The query builder used to create (named parameter) prepared statements.
+	 */
+	void setQueryBuilder(IQueryBuilder qb);
+}
---
+++ tags/yapool/0.9.1/yapool-examples/src/main/resources/example-qc-struct.sql
@@ -0,0 +1,4 @@
+--[]
+create table t 
+(id integer generated always as identity(start with 100) primary key, 
+name varchar(256));

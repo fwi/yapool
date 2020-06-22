0a352ba00e3fada293778be99e147b9148cfa167
fwi <frederik.wiers@gmail.com>
2020-06-22 13:38:02 +0200

    Poolsmap documentation.

--- src/main/java/com/github/fwi/yapool/IPoolsMapFactory.java
+++ src/main/java/com/github/fwi/yapool/IPoolsMapFactory.java
@@ -4,11 +4,12 @@ package com.github.fwi.yapool;
  * A factory for pools for a {@link PoolsMap}.
  * <br>Note that the pool-key is the only "given" with which a pool can be created,
  * as such the pool-key needs to contain enough information to create a properly configured pool.
+ * <br>At the same time, the pool-key must function as a proper key and therefor must implement
+ * proper {@link Object#hashCode()} and {@link Object#equals(Object)} methods
+ * that correctly identify two pool-key objects as the same when they are the same. 
  *
  * @param <T> The type of pool-resource.
  * @param <K> The type of pool-key.
- * 
- * TODO: create base class for this interface?
  * 
  */
 public interface IPoolsMapFactory<T, K> {
@@ -17,6 +18,7 @@ public interface IPoolsMapFactory<T, K> {
 	 * Creates a pruned pool.
 	 * Creating a pool should be fast, returned pool should be empty and must be opened.
 	 * The pool must have a prune-interval and max. idle timeout greater than 0.
+	 * The pool must have been added to the pool-pruner for pruning.
 	 * <br>If a pool cannot be created, a {@link RuntimeException} must be thrown.
 	 */
 	PrunedPool<T> create(K poolKey, PoolPruner poolPruner);
